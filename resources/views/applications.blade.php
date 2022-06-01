ecf2aaf10696f86113b3522d83fa2a09e96e2c38
ynsoft-dev <ynsoft@live.fr>
2022-06-01 01:42:18 +0100

    mise en place des application au format html

---
+++ resources/views/applications.blade.php
@@ -0,0 +1,128 @@
+{{-- @section('content') --}}
+<div class="wrapper">
+
+    <div class="row">
+        <div class="cols">
+            <!-- carte GLPI -->
+            <div class="tuile" ontouchstart="this.classList.toggle('click');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
+                        <div class="inner">
+                            <p>Services Center</p>
+                            <span>GLPI</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Exprimez vos besoins et contraintes informatiques</p>
+                            <a href="http://servicecenter.cevital.com/" target="blank">GLPI</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- carte Kelio -->
+            <div class="tuile" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/501/)">
+                        <div class="inner">
+                            <p>Pointage et Incidences</p>
+                            <span>Kelio</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Gérer votre pointage et incidences</p>
+                            <a href="http://kelio/open/login" target="blank">Kelio</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- carte Coswin -->
+            <div class="tuile" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/502/)">
+                        <div class="inner">
+                            <p>GMAO</p>
+                            <span>Coswin</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Gestion des interventions maintenance et bien plus</p>
+                            <a href="http://gmao/coswin/Login.xhtml" target="blank">Coswin</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- carte Annuaire Téléphonique -->
+            <div class="tuile" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/503/)">
+                        <div class="inner">
+                            <p>Annuaire Téléphonique</p>
+                            <span>Filliale</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Liste des postes téléphoniques de la filliale</p>
+                            <a href="http://portail-annuaire/index.php" target="blank">Annuaire</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- carte Kelio -->
+            <div class="tuile" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/501/)">
+                        <div class="inner">
+                            <p>Pointage et Incidences</p>
+                            <span>Kelio</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Gérer votre pointage et incidences</p>
+                            <a href="http://kelio/open/login" target="blank">Kelio</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- carte Coswin -->
+            <div class="tuile" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/502/)">
+                        <div class="inner">
+                            <p>GMAO</p>
+                            <span>Coswin</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Gestion des interventions maintenance et bien plus</p>
+                            <a href="http://gmao/coswin/Login.xhtml" target="blank">Coswin</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- carte Annuaire Téléphonique -->
+            <div class="tuile" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/503/)">
+                        <div class="inner">
+                            <p>Annuaire Téléphonique</p>
+                            <span>Filliale</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Liste des postes téléphoniques de la filliale</p>
+                            <a href="http://portail-annuaire/index.php" target="blank">Annuaire</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- @endsection --}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,11 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
         @include('layouts.nav')
-
+        
+        @include('applications')
 
     </div>
 </body>
